a897075e44826925441dcb513f6eec4df595b05f
Ryan Schlesinger <ryan@instanceinc.com>
2014-04-18 19:19:54 -0700

    Remove s3_enabled attribute

--- attributes/ace.rb
+++ attributes/ace.rb
@@ -2,6 +2,5 @@ default['yum']['ace']['repositoryid'] = 'ace'
 default['yum']['ace']['description'] = 'ace packages'
 default['yum']['ace']['baseurl'] = 'http://ace-opsworks-packages.s3.amazonaws.com/$releasever'
 default['yum']['ace']['enabled'] = true
-default['yum']['ace']['s3_enabled'] = true
 default['yum']['ace']['gpgcheck'] = false
 default['yum']['ace']['priority'] = '2'
--- recipes/default.rb
+++ recipes/default.rb
@@ -3,7 +3,6 @@ yum_repository 'ace' do
   description node['yum']['ace']['description']
   baseurl node['yum']['ace']['baseurl']
   enabled node['yum']['ace']['enabled']
-  s3_enabled node['yum']['ace']['s3_enabled']
   gpgcheck node['yum']['ace']['gpgcheck']
   priority node['yum']['ace']['priority']
 
